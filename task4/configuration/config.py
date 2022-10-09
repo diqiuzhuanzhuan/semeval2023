d9c8180b1df8e3c78cf1487f144b63fa101f21f4
diqiuzhuanzhuan <diqiuzhuanzhuan@gmail.com>
2022-10-09 16:55:25 +0800

    add data and README

--- task4/configuration/config.py
+++ task4/configuration/config.py
@@ -1,3 +1,9 @@
 # -*- coding: utf-8 -*-
 # author: Feynman
 # email: diqiuzhuanzhuan@gmail.com
+import os
+import pathlib
+
+root_path = pathlib.Path(os.path.pardir(os.path.abspath(__file__)))
+data_path = root_path/'data'
+log_path = root_path/'logs'
