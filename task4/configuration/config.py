7c4b22fbc12f8967976d7ac1b45c031424bde180
diqiuzhuanzhuan <diqiuzhuanzhuan@gmail.com>
2022-10-13 12:10:52 +0800

    update requirements and add metadata and read arguments code

--- task4/configuration/config.py
+++ task4/configuration/config.py
@@ -3,8 +3,19 @@
 # email: diqiuzhuanzhuan@gmail.com
 import os
 import pathlib
+from typing import NamedTuple
 
-root_path = pathlib.Path(os.path.pardir(os.path.abspath(__file__)))
+root_path = pathlib.Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 data_path = root_path/'data'
 log_path = root_path/'logs'
 output_path = root_path/'output'
+
+train_data_path = data_path/'training_data'
+
+
+
+train_file = {
+    'arguments': train_data_path/'arguments-training.tsv',
+    'labels': train_data_path/'labels-training.tsv',
+    'level1-labels': train_data_path/'level1-labels-training.tsv'
+}
--- task4/data_man/meta_data.py
+++ task4/data_man/meta_data.py
@@ -4,7 +4,10 @@
 
 
 from dataclasses import dataclass
-from typing import Dict
+from typing import Dict, List, Union
+import os
+import pandas as pd
+from pathlib import Path
 
 
 @dataclass
@@ -24,3 +27,45 @@ class ArgumentItem:
             stance=json_dict['Stance'],
             premise=json_dict['Premise']
         )
+
+
+@dataclass
+class ArgumentDataset:
+    """_summary_
+    """
+    dataset_path: str
+    arguments: List[ArgumentItem]
+
+
+@dataclass        
+class LabelItem:
+    """_summary_
+    """
+    argument_id: str
+    label: List[int]
+
+    @classmethod
+    def from_dict(cls, json_dict: Dict):
+        return LabelItem(
+            argument_id=json_dict['Argument ID'],
+            label=json_dict['label']
+        )
+        
+
+@dataclass 
+class LabelDataset:
+    """_summary_
+    """
+    labels_name: List[str]
+    labels: List[LabelItem]
+    
+def read_arguments_from_file(file: Union[bytes, str, os.PathLike]) -> List[ArgumentItem]:
+    data = pd.read_csv(Path(file).as_posix(), delimiter='\t')
+    print('read {} arguments from {}.'.format(len(data), str(file)))
+    return [ArgumentItem.from_dict(data.iloc[i]) for i in range(len(data))]
+
+if __name__ == "__main__":
+    from task4.configuration import config
+    arguments = read_arguments_from_file(config.train_file['arguments'])
+    print(arguments[0])
+    
