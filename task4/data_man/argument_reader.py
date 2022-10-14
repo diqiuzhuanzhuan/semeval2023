e3e92dc94d565d5c9ebd8d0789214ca9347d8826
diqiuzhuanzhuan <diqiuzhuanzhuan@gmail.com>
2022-10-14 19:50:10 +0800

    make collate_batch function run perfectly even in the situation no matter where label is existed or not

--- task4/data_man/argument_reader.py
+++ task4/data_man/argument_reader.py
@@ -95,23 +95,28 @@ class ArgumentsDataModule(pl.LightningDataModule):
 
         self.stage = stage
 
+
     def collate_batch(self, batch):
         batch_size = len(batch)
         batch_ = list(zip(*batch))
         argument_id, input_ids_batch, _, _, label_ids_batch = batch_
         max_len = max([len(_) for _ in input_ids_batch])
-        max_label_len = max([len(_) for _ in label_ids_batch])
         input_ids_tensor = torch.empty(size=[batch_size, max_len], dtype=torch.long).fill_(0)
         token_type_ids_tensor = torch.empty(size=[batch_size, max_len], dtype=torch.long).fill_(0)
         attention_mask_tensor = torch.empty(size=[batch_size, max_len], dtype=torch.long).fill_(0)
-        label_ids_tensor = torch.empty(size=[batch_size, max_label_len], dtype=torch.long).fill_(0)
+        if label_ids_batch:
+            max_label_len = max([len(_) for _ in label_ids_batch])
+            label_ids_tensor = torch.empty(size=[batch_size, max_label_len], dtype=torch.long).fill_(0)
+        else:
+            label_ids_tensor = None
         for i in range(batch_size):
             _, input_ids, token_type_ids, attention_mask, label_ids = batch[i]
             available_length = len(input_ids)
             input_ids_tensor[i][0:available_length] = torch.tensor(input_ids, dtype=torch.long)
             token_type_ids_tensor[i][0:available_length] = torch.tensor(token_type_ids, dtype=torch.long)
             attention_mask_tensor[i][0:available_length] = torch.tensor(attention_mask, dtype=torch.long)
-            label_ids_tensor[i] = torch.tensor(label_ids, dtype=torch.long)
+            if label_ids_tensor is not None:
+                label_ids_tensor[i] = torch.tensor(label_ids, dtype=torch.long)
 
         return argument_id, input_ids_tensor, token_type_ids_tensor, attention_mask_tensor, label_ids_tensor
 
