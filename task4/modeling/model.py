f169026621357044460e3b622306f3eeaf6d0542
diqiuzhuanzhuan <diqiuzhuanzhuan@gmail.com>
2022-10-17 11:29:45 +0800

    add model framework

--- task4/modeling/model.py
+++ task4/modeling/model.py
@@ -3,7 +3,30 @@
 # email: diqiuzhuanzhuan@gmail.com
 
 
-class ArgumentModel(object):
+from allennlp.common.registrable import Registrable
+from allennlp.common.params import Params
+import pytorch_lightning as pl
+from typing import AnyStr, Any, Union, Optional
+from transformers import AutoModel
 
-    def __init__(self) -> None:
-        pass
+
+class ArgumentModel(Registrable):
+    pass
+
+@ArgumentModel.register('baseline_argument_model') 
+class BaselineArgumentModel(pl.LightningModule, ArgumentModel):
+    
+    def __init__(
+        self, 
+        encoder_model: AnyStr='bert-base-uncased'
+        ) -> None:
+        super().__init__()
+        self.encoder = AutoModel.from_pretrained(encoder_model)
+
+
+if __name__ == '__main__':
+    params = Params({
+        'type': 'baseline_argument_model',
+        'encoder_model': 'bert-base-uncased'         
+    })
+    argument_model = ArgumentModel.from_params(params=params)
