b2d5964b29ebf0e7128a111ad0ea180e03c34bee
diqiuzhuanzhuan <diqiuzhuanzhuan@gmail.com>
2022-10-17 16:40:45 +0800

    add warmup

--- task4/modeling/model.py
+++ task4/modeling/model.py
@@ -8,8 +8,9 @@ from allennlp.common.registrable import Registrable
 from allennlp.common.params import Params
 import pytorch_lightning as pl
 from typing import AnyStr, Any, Union, Optional
-from transformers import AutoModel, create_optimizers
+from transformers import AutoModel
 import torch
+import functools
 
 
 class ArgumentModel(Registrable):
@@ -32,8 +33,33 @@ class BaselineArgumentModel(pl.LightningModule, ArgumentModel):
         self.lr = lr
 
     def configure_optimizers(self):
-        optimizer = torch.optim.AdamW(self.parameters, lr=self.lr)
-        return optimizer
+        optimizer = torch.optim.AdamW(self.parameters(), lr=self.lr, weight_decay=0.01)
+
+        warmup_steps = self.warmup_steps
+        def fn(warmup_steps, step):
+            if step < warmup_steps:
+                return float(step) / float(max(1, warmup_steps))
+            else:
+                return 1.0
+
+        def linear_warmup_decay(warmup_steps):
+            return functools.partial(fn, warmup_steps)
+
+        scheduler = {
+            "scheduler": torch.optim.lr_scheduler.LambdaLR(
+                optimizer,
+                linear_warmup_decay(warmup_steps),
+            ),
+            "interval": "step",
+            "frequency": 1,
+        }
+
+        return [optimizer], [scheduler]
+
+
+    def compute_loss(self, logits, targets):
+        loss = torch.nn.BCEWithLogitsLoss()(logits, targets)
+        return loss
 
     def forward_step(self, batch):
         argument_id, input_ids, token_type_ids, attention_mask, label_ids = batch
@@ -50,7 +76,7 @@ class BaselineArgumentModel(pl.LightningModule, ArgumentModel):
             'predict': predict
         }
         if label_ids:
-            loss = torch.nn.BCEWithLogitsLoss()(logits, label_ids)
+            loss = self.compute_loss(logits, label_ids)
             return_dict['loss'] = loss
 
         return return_dict
